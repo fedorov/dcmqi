b91eab03aacf3772670470b94ce32ac10aceb27a
Andrey Fedorov <andrey.fedorov@gmail.com>
2017-03-14 12:18:05 -0400

    ENH: a different shot at refactoring

--- include/dcmqi/Helper.h
+++ include/dcmqi/Helper.h
@@ -19,7 +19,6 @@
 
 using namespace std;
 
-
 namespace dcmqi {
 
 
@@ -50,6 +49,9 @@ namespace dcmqi {
     static void checkValidityOfFirstSrcImage(DcmSegmentation *segdoc);
 
     static CodeSequenceMacro* createNewCodeSequence(const string& code, const string& designator, const string& meaning);
+
+    static OFString generateUID();
+    static OFString getTagAsOFString(DcmDataset*, DcmTagKey);
   };
 
 }
