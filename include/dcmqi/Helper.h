ef37848a6ebb2ff14a0d7693b4f3fa5523bd720b
Andrey Fedorov <andrey.fedorov@gmail.com>
2017-03-15 21:12:33 -0400

    ENH: adding functionality - temp commit

--- include/dcmqi/Helper.h
+++ include/dcmqi/Helper.h
@@ -17,6 +17,8 @@
 // DCMQI includes
 #include "dcmqi/Exceptions.h"
 
+#include <json/json.h>
+
 using namespace std;
 
 namespace dcmqi {
@@ -45,6 +47,9 @@ namespace dcmqi {
 
     static CodeSequenceMacro stringToCodeSequenceMacro(string str);
     static DSRCodedEntryValue stringToDSRCodedEntryValue(string str);
+    static string codeSequenceMacroToString(CodeSequenceMacro);
+
+    static CodeSequenceMacro jsonToCodeSequenceMacro(Json::Value);
 
     static void checkValidityOfFirstSrcImage(DcmSegmentation *segdoc);
 
